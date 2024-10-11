632e9aa709ffe8c5100730ff3754b79766e722bd
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-10-11 14:47:35 +0300

    fix

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -48,7 +48,7 @@ test('Login with valid credentials', async ({ page }) => {
   await page.$('a[href="/catalog"]');
   expect(page.url()).toBe('http://localhost:3000/catalog');
 });
-
+ 
 test('Login with empty input fields', async ({ page }) => {
   await page.goto('http://localhost:3000/login');
   await page.click('input[type="submit"]');
